7440316596b48ddc73b829812cd4948f3138f454
Nathan Bland <nathan.bland@gmail.com>
2014-09-16 08:59:21 +0000

    added template folder with working version

---
+++ templates/loadContacts.js
@@ -0,0 +1,16 @@
+var templateSource = document.getElementById('contact-template').innerHTML;
+
+var contactTemplate = Handlebars.compile( templateSource );
+
+function createContactFromTemplate( contact ) {
+    return contactTemplate( contact );
+}
+
+function renderContactsWithTemplate( contacts ) {
+    var container = document.querySelector('.contacts-container');
+    contacts.map( createContactFromTemplate ).forEach(function( contactHTML ){
+    container.innerHTML += contactHTML;
+    });
+}
+
+renderContactsWithTemplate(window.contacts);
