195c9b341df8d705adcf033773363752ed175fde
Nathan Bland <nathan.bland@gmail.com>
2014-09-18 05:24:50 +0000

    added working section with jsonp

---
+++ JSONP/contacts.js
@@ -0,0 +1,181 @@
+renderContacts([{
+    "id": 1,
+    "firstName": "Nathan",
+    "lastName": "Bland",
+    "birthday": "1992-08-21T00:00:00.00",
+    "phoneNumbers": [{
+        "id": 1,
+        "phoneNumber": 5585558555,
+        "type": "Cell"
+    },{ 
+        "id": 1,
+        "phoneNumber": 7775551234,
+        "type": "Home"
+    }],
+    "address": [{
+        "id": 1,
+        "desc": "school",
+        "street": "3800 S. 48th Str.",
+        "city": "Lincoln",
+        "state": "NE",
+        "zip": 68506},
+        {
+        "id": 2,
+        "desc": "Work",
+        "street": "3800 S. 48th Str.",
+        "city": "Lincoln",
+        "state": "NE",
+        "zip": 68506}],
+    "avatar": "../images/contacts/default.png"
+},
+{
+    "id": 2,
+    "firstName": "John",
+    "lastName": "Smith",
+    "birthday": "1990-10-13T00:00:00.00",
+    "phoneNumbers": [{
+        "id": 2,
+        "phoneNumber": 1112223333,
+        "type": "Home"
+    }],
+    "address": [],
+    "avatar": "../images/contacts/default.png"},
+{
+    "id": 3,
+    "firstName": "Mike",
+    "lastName": "",
+    "birthday": "",
+    "phoneNumbers": [{
+        "id": 3,
+        "phoneNumber": 5554449876,
+        "type": "Work"
+    }],
+    "address": [{
+        "id": 3,
+        "desc": "Home",
+        "street": "1234 Somewhere street",
+        "city": "nowhere",
+        "state": "NE",
+        "zip": 68555}],
+    "avatar": "../images/contacts/default.png"
+},
+{
+    "id": 4,
+    "firstName": "mark",
+    "lastName": "John",
+    "birthday": "",
+    "phoneNumbers": [{
+        "id": 4,
+        "phoneNumber":3216541234,
+        "type": "Home"
+    }],
+    "address": [],
+    "avatar": "../images/contacts/default.png"
+},
+{
+    "id": 5,
+    "firstName": "Mario",
+    "lastName": "Plumber",
+    "birthday": "",
+    "phoneNumbers": [{
+        "id": 5,
+        "phoneNumber": 5460007894,
+        "type": "Other"
+    }],
+    "address": [{
+        "id": 5,
+        "desc": "office",
+        "street": "32 S. Magic str.",
+        "city": "Kansas City",
+        "state": "KS",
+        "zip": 45612}],
+    "avatar": "../images/contacts/default.png"
+},
+{
+    "id": 6,
+    "firstName": "Malachi",
+    "lastName": "",
+    "birthday": "",
+    "phoneNumbers": [{
+        "id": 6,
+        "phoneNumber": 4021112222,
+        "type": "Cell"
+    }],
+    "address": [],
+    "avatar": "../images/contacts/default.png"
+},
+{
+    "id": 7,
+    "firstName": "Keith",
+    "lastName": "",
+    "birthday": "",
+    "phoneNumbers": [{
+        "id": 7,
+        "phoneNumber": 9998884561,
+        "type": "Cell"
+    }],
+    "address": [{
+        "id": 7,
+        "desc": "Home away from Home",
+        "street": "3800 S. 48th Str.",
+        "city": "Lincoln",
+        "state": "NE",
+        "zip": 68506}],
+    "avatar": "../images/contacts/default.png"
+},
+{
+    "id": 8,
+    "firstName": "Brad",
+    "lastName": "",
+    "birthday": "",
+    "phoneNumbers": [{
+        "id": 8,
+        "phoneNumber": 1110012134,
+        "type": "Home"
+    }],
+    "address": [{
+        "id": 8,
+        "desc": "Home away from Home",
+        "street": "3800 S. 48th Str.",
+        "city": "Lincoln",
+        "state": "NE",
+        "zip": 68506}],
+    "avatar": "../images/contacts/default.png"
+},
+{
+    "id": 9,
+    "firstName": "David",
+    "lastName": "",
+    "birthday": "",
+    "phoneNumbers": [{
+        "id": 9,
+        "phoneNumber": 12346541237,
+        "type": "Backup Phone"
+    }],
+    "address": [{
+        "id": 9,
+        "desc": "Home away from Home",
+        "street": "3800 S. 48th Str.",
+        "city": "Lincoln",
+        "state": "NE",
+        "zip": 68506}],
+    "avatar": "../images/contacts/default.png"},
+{
+    "id": 10,
+    "firstName": "Chad",
+    "lastName": "",
+    "birthday": "1993-04-03T00:00:00.00",
+    "phoneNumbers": [{
+        "id": 10,
+        "phoneNumber": 3034567891,
+        "type": "Mobile"
+    }],
+    "address": [{
+        "id": 10,
+        "desc": "Home away from Home",
+        "street": "3800 S. 48th Str.",
+        "city": "Lincoln",
+        "state": "NE",
+        "zip": 68506}],
+    "avatar": "../images/contacts/default.png"}
+]);
